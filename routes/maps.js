30732d59e5c10ad363cdd2ef73c6844c5cdc2eda
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2021-10-18 04:03:18 +0000

    Amans work on POST DB request - post requested feeding into DB but map_id and user_id fix needed + dynamically need to add points

--- routes/maps.js
+++ routes/maps.js
@@ -9,47 +9,64 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    let query = `SELECT * FROM maps`;
-    console.log(query);
-    db.query(query)
-      .then(data => {
-        const maps = data.rows;
-        res.json({ maps });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-
-  router.get('/create', (req, res) => {
-    res.render("create_map");
-  });
-
-  router.post('/create', (req, res) => {
-    console.log("req.body",req.body);
-  //   const markers = req.body.markers;
-  //   const mapTitle = req.body.mapTitle;
-  //   const mapDesc = req.body.mapDescription;
-
-  //   const queryStringMaps = `INSERT INTO maps (title, description, user_id, preview_image) VALUES ($1, $2, $3, $4) RETURNING *;`
-  //   const queryStringPoints = `INSERT INTO points (title, description, latitude, longtitude, map_id, user_id, category) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`
-  //   const valuesPoints = [`${markers[0].pointTitle}`, `${markers[0].pointDescription}`, `${markers[0].coordinates.lat}`, `${markers[0].coordinates.lng}`,"food"]
-  //   const valuesMaps = [`${mapTitle}`, `${mapDesc}`,'1', ]
-  //   db.query(queryStringMaps,Valuesmap )
+  // router.get("/", (req, res) => {
+  //   let query = `SELECT * FROM maps`;
+  //   console.log(query);
+  //   db.query(query)
   //     .then(data => {
   //       const maps = data.rows;
-  //       // res.json({ maps });
-  //       res.render("create_map");
+  //       res.json({ maps });
   //     })
   //     .catch(err => {
   //       res
   //         .status(500)
   //         .json({ error: err.message });
   //     });
-  //   db.query(queryStringPoints,valuesPoints)
+  // });
+
+  router.get('/create', (req, res) => {
+    res.render("create_map");
   });
+
+  router.post('/create', (req, res) => {
+    // console.log("req.body",req.body)
+    const markers = req.body.markers;
+    const mapTitle = req.body.mapTitle;
+    const mapDesc = req.body.mapDescription;
+
+    const queryStringMaps = `INSERT INTO maps (title, description, user_id, preview_image) VALUES ($1, $2, $3, $4) RETURNING *`;
+    const valuesMaps = [`${mapTitle}`, `${mapDesc}`, '1', 'https://i.pinimg.com/474x/b4/7b/96/b47b9623ba93546b9a2c412e1abe9306.jpg'];
+
+    // for (let i = 0; i < markers; i++ {markers[i].pointTitle ... etc etc})
+    const queryStringPoints = `INSERT INTO points (title, description, latitude, longitude, category) VALUES ($1, $2, $3, $4, $5) RETURNING *`;
+    const valuesPoints = [`${markers[3].pointTitle}`, `${markers[3].pointDescription}`, `${markers[3].coordinates.lat}`, `${markers[3].coordinates.lng}`, `${markers[3].category}`];
+
+    db.query(queryStringMaps, valuesMaps) //query+insert map data first
+      .then(data => {
+        const map = data.rows;
+        console.log('mapsDB:', map)
+        db.query(queryStringPoints, valuesPoints) //query+insert points data second. inside promise.then to ensure that this chains after map creation
+        .then(data => {
+          const points = data.rows;
+          console.log('pointsDB:', points)
+        })
+        .catch(err => {
+          console.error('points_err:', err.message);
+          res
+            .status(500)
+        });
+      })
+      .catch(err => {
+        console.error('map_err:', err.message);
+        res
+          .status(500)
+      });
+
+
+
+
+  });
+
   return router;
+
 };
