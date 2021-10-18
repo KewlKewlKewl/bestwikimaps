1ea52bdf2ccf2dc2e4bceb02e7e1f80505389091
C W <claudiaywong@msn.com>
2021-10-18 00:21:38 +0000

    added categories to point form submission

--- routes/maps.js
+++ routes/maps.js
@@ -28,14 +28,15 @@ module.exports = (db) => {
     res.render("create_map");
   });
 
-  // router.post('/create', (req, res) => {
-  //   console.log("req.body",req.body);
+  router.post('/create', (req, res) => {
+    console.log("req.body",req.body);
   //   const markers = req.body.markers;
   //   const mapTitle = req.body.mapTitle;
   //   const mapDesc = req.body.mapDescription;
+
   //   const queryStringMaps = `INSERT INTO maps (title, description, user_id, preview_image) VALUES ($1, $2, $3, $4) RETURNING *;`
   //   const queryStringPoints = `INSERT INTO points (title, description, latitude, longtitude, map_id, user_id, category) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`
-  //   const valuesPoints = [`${markers[0].pointTitle}`, `${markers[0].pointDescription}`, `${markers[0].coordinates.lat}`, `${markers[0].coordinates.lng}`,"restaurants"]
+  //   const valuesPoints = [`${markers[0].pointTitle}`, `${markers[0].pointDescription}`, `${markers[0].coordinates.lat}`, `${markers[0].coordinates.lng}`,"food"]
   //   const valuesMaps = [`${mapTitle}`, `${mapDesc}`,'1', ]
   //   db.query(queryStringMaps,Valuesmap )
   //     .then(data => {
@@ -49,6 +50,6 @@ module.exports = (db) => {
   //         .json({ error: err.message });
   //     });
   //   db.query(queryStringPoints,valuesPoints)
-  // });
+  });
   return router;
 };
