8b92419774147dabcc5a10b6a47442a432bd9c92
C W <claudiaywong@msn.com>
2021-10-18 00:00:19 +0000

    added POST createmap

--- routes/maps.js
+++ routes/maps.js
@@ -1,7 +1,7 @@
 /*
- * All routes for Widgets are defined here
- * Since this file is loaded in server.js into api/widgets,
- *   these routes are mounted onto /widgets
+ * All routes for maps are defined here
+ * Since this file is loaded in server.js into api/maps,
+ *   these routes are mounted onto /maps
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
@@ -10,12 +10,12 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    let query = `SELECT * FROM widgets`;
+    let query = `SELECT * FROM maps`;
     console.log(query);
     db.query(query)
       .then(data => {
-        const widgets = data.rows;
-        res.json({ widgets });
+        const maps = data.rows;
+        res.json({ maps });
       })
       .catch(err => {
         res
@@ -23,5 +23,32 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  router.get('/create', (req, res) => {
+    res.render("create_map");
+  });
+
+  // router.post('/create', (req, res) => {
+  //   console.log("req.body",req.body);
+  //   const markers = req.body.markers;
+  //   const mapTitle = req.body.mapTitle;
+  //   const mapDesc = req.body.mapDescription;
+  //   const queryStringMaps = `INSERT INTO maps (title, description, user_id, preview_image) VALUES ($1, $2, $3, $4) RETURNING *;`
+  //   const queryStringPoints = `INSERT INTO points (title, description, latitude, longtitude, map_id, user_id, category) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`
+  //   const valuesPoints = [`${markers[0].pointTitle}`, `${markers[0].pointDescription}`, `${markers[0].coordinates.lat}`, `${markers[0].coordinates.lng}`,"restaurants"]
+  //   const valuesMaps = [`${mapTitle}`, `${mapDesc}`,'1', ]
+  //   db.query(queryStringMaps,Valuesmap )
+  //     .then(data => {
+  //       const maps = data.rows;
+  //       // res.json({ maps });
+  //       res.render("create_map");
+  //     })
+  //     .catch(err => {
+  //       res
+  //         .status(500)
+  //         .json({ error: err.message });
+  //     });
+  //   db.query(queryStringPoints,valuesPoints)
+  // });
   return router;
 };
--- server.js
+++ server.js
@@ -54,9 +54,9 @@ app.get('/', (req, res) => {
   res.render("index");
 });
 
-app.get('/maps/create', (req, res) => {
-  res.render("create_map");
-})
+// app.get('/maps/create', (req, res) => {
+//   res.render("create_map");
+// })
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
