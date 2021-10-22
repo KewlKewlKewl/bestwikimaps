604008f5f12527cc4dc51ce60e97c8c620e8a6df
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2021-10-22 02:49:29 +0000

    created new seed data and made x2 map demos for Calgary

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,12 +1,15 @@
 -- -- Maps table seeds here (Example)
 INSERT INTO maps (id, title, description, user_id, preview_image)
-VALUES ('xbY123', 'New Map 1', 'This is test map 1', 1, 'https://geospatialmedia.s3.amazonaws.com/wp-content/uploads/2019/07/Apple-maps-app.png');
+VALUES ('xbY123', 'Best Calgary Sights of 2020', 'This is test map 1', 1, 'https://geospatialmedia.s3.amazonaws.com/wp-content/uploads/2019/07/Apple-maps-app.png');
 
 INSERT INTO maps (id, title, description, user_id, preview_image)
-VALUES ('xbY124', 'New Map 2', 'This is test map 2', 1, 'https://geospatialmedia.s3.amazonaws.com/wp-content/uploads/2019/07/Apple-maps-app.png');
+VALUES ('jIzfWb', 'Faezs Must See Spots!', 'My must see spots in Calgary.', 1, 'https://geospatialmedia.s3.amazonaws.com/wp-content/uploads/2019/07/Apple-maps-app.png');
 
 INSERT INTO maps (id, title, description, user_id, preview_image)
-VALUES ('xbY125', 'New Map 3', 'This is test map 3', 1, 'https://geospatialmedia.s3.amazonaws.com/wp-content/uploads/2019/07/Apple-maps-app.png');
+VALUES ('C5Bz2C', 'Faezs Favourite Food Spots', 'The best food spots in Calgary.', 1, 'https://geospatialmedia.s3.amazonaws.com/wp-content/uploads/2019/07/Apple-maps-app.png');
 
 INSERT INTO maps (id, title, description, user_id, preview_image)
-VALUES ('xbY126', 'New Map 4', 'This is test map 4', 1, 'https://geospatialmedia.s3.amazonaws.com/wp-content/uploads/2019/07/Apple-maps-app.png');
+VALUES ('xbY124', 'Vancouver in the Winter', 'This is test map 2', 1, 'https://geospatialmedia.s3.amazonaws.com/wp-content/uploads/2019/07/Apple-maps-app.png');
+
+INSERT INTO maps (id, title, description, user_id, preview_image)
+VALUES ('xbY125', 'Toronto 2019', 'This is test map 3', 1, 'https://geospatialmedia.s3.amazonaws.com/wp-content/uploads/2019/07/Apple-maps-app.png');
--- db/seeds/03_points.sql
+++ db/seeds/03_points.sql
@@ -13,6 +13,30 @@ INSERT INTO points (title, description, latitude, longitude, map_id, user_id, ca
 VALUES ('Point 4', 'Point Description 4', '-101.4', '30.3', 'xbY123', 1, 'Food', 'https://i.pinimg.com/474x/b4/7b/96/b47b9623ba93546b9a2c412e1abe9306.jpg');
 
 INSERT INTO points (title, description, latitude, longitude, map_id, user_id, category, point_image)
+VALUES ('3 Dogs Trail', 'Great mini trail for the hikers out there. Beautiful nature walk.', '51.020764', '-114.135171', 'jIzfWb', 1, 'Nature', 'https://images.unsplash.com/photo-1600284536251-8bb98db53468?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dHJhaWx8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80');
+
+INSERT INTO points (title, description, latitude, longitude, map_id, user_id, category, point_image)
+VALUES ('Bobs Burgers', 'Great spot to eat before seeing the Flames lose.', '51.02447', '-114.114113', 'jIzfWb', 1, 'Food', 'https://previews.123rf.com/images/jetsam86/jetsam862003/jetsam86200300060/141261819-beer-and-snacks-set-pub-restaurant-bar-food.jpg');
+
+INSERT INTO points (title, description, latitude, longitude, map_id, user_id, category, point_image)
+VALUES ('Blue Johns', 'Good pub/club for a nightout. Avoid the Knoxville Tavern down the street.', '51.045065', '-114.082971', 'jIzfWb', 1, 'Nightlife', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcz0xhRNst_Wfwb2OLtWn9qTDqozAHt7iUqg&usqp=CAU');
+
+INSERT INTO points (title, description, latitude, longitude, map_id, user_id, category, point_image)
+VALUES ('Pinebrook Golf Course', 'The go to course to play 18 in Calgary. Golf course plus a view!', '51.03086', '-114.245571', 'jIzfWb', 1, 'Nature', 'https://www.perthgolf.com/wp-content/uploads/2020/08/drone2.jpg');
+
+INSERT INTO points (title, description, latitude, longitude, map_id, user_id, category, point_image)
+VALUES ('Jane Novelties', 'Cool little thrift store with unique clothes.', '51.039091', '-114.144778', 'jIzfWb', 1, 'Shopping', 'https://manofmany.com/wp-content/uploads/2020/05/Feature-Image.png');
+
+INSERT INTO points (title, description, latitude, longitude, map_id, user_id, category, point_image)
+VALUES ('Minami', 'Fine dining sushi. Be ready to pay a price but worth it, trust me!', '51.047636', '-114.086124', 'C5Bz2C', 1, 'Food', 'https://cdn.vox-cdn.com/thumbor/3OdmXqvbG3rq2lKOagEjaExkpis=/0x0:2048x1588/1200x800/filters:focal(861x631:1187x957)/cdn.vox-cdn.com/uploads/chorus_image/image/68617826/134301527_150986323476283_8160139784797337975_o.0.jpg');
+
+INSERT INTO points (title, description, latitude, longitude, map_id, user_id, category, point_image)
+VALUES ('Two Peaks Diner', 'Best pastries in the city. Try the apple pie.', '51.042537', '-114.095579', 'C5Bz2C', 1, 'Food', 'https://www.chewoutloud.com/wp-content/uploads/2012/12/apple-pie-2.jpg');
+
+INSERT INTO points (title, description, latitude, longitude, map_id, user_id, category, point_image)
+VALUES ('Sopranos', 'Amazing pizza by the slice. Great quality; try the neopolitan.', '51.047571', '-114.141766', 'C5Bz2C', 1, 'Food', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrXd4VTPgIJ6JMd8nxEK6hXk63MDkJ2dWp6Q&usqp=CAU');
+
+INSERT INTO points (title, description, latitude, longitude, map_id, user_id, category, point_image)
 VALUES ('Point 1', 'Point Description 1', '-71.4' , '30.3', 'xbY124', 1, 'Food', 'https://i.pinimg.com/474x/b4/7b/96/b47b9623ba93546b9a2c412e1abe9306.jpg');
 
 INSERT INTO points (title, description, latitude, longitude, map_id, user_id, category, point_image)
@@ -35,3 +59,4 @@ VALUES ('Point 3', 'Point Description 3', '-91.4', '30.3', 'xbY125', 1, 'Food', 
 
 INSERT INTO points (title, description, latitude, longitude, map_id, user_id, category, point_image)
 VALUES ('Point 4', 'Point Description 4', '-101.4', '30.3', 'xbY125', 1, 'Food', 'https://i.pinimg.com/474x/b4/7b/96/b47b9623ba93546b9a2c412e1abe9306.jpg');
+
